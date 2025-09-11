758e51bd731a50767f9fb1e23220cf6ba5ff7c2a
igorwolfs <wolfs.igor@hotmail.com>
2025-09-11 19:48:52 +0200

    adc working on i2c bus

--- notes.md
+++ notes.md
@@ -1,4 +1,8 @@
 # ADC - ADS51115IDGS
+- Nucleo connected pins
+	- SCL: D15 - PB8 - CN10 pin 3
+	- SDA: D14 - PB9 - CN10 pin 5
+
 ## I2C
 - Address: 1001000
 
--- nucleo-piezodriver/Core/Src/main.c
+++ nucleo-piezodriver/Core/Src/main.c
@@ -21,7 +21,9 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "ads1115.h"
+#include <stdio.h>
+#define ARRAY_SIZE(n)   (sizeof(n) / sizeof(n[0]))
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -40,6 +42,7 @@
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
+I2C_HandleTypeDef hi2c1;
 
 /* USER CODE BEGIN PV */
 
@@ -48,12 +51,29 @@
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
+static void MX_I2C1_Init(void);
 /* USER CODE BEGIN PFP */
-
+void SWO_Init(void) {
+  uint32_t cpuCoreFreqHz = 16e6;
+  uint32_t portBits = 0x1;
+  
+  uint32_t SWOSpeed = 2e6; // default 64k baud rate, changed
+  uint32_t SWOPrescaler = (cpuCoreFreqHz / SWOSpeed) - 1; /* SWOSpeed in Hz, note that cpuCoreFreqHz is expected to be match the CPU core clock */
+  CoreDebug->DEMCR = CoreDebug_DEMCR_TRCENA_Msk; /* enable trace in core debug */
+  *((volatile unsigned *)(ITM_BASE + 0x400F0)) = 0x00000002; /* "Selected PIN Protocol Register": Select which protocol to use for trace output (2: SWO NRZ, 1: SWO Manchester encoding) */
+  *((volatile unsigned *)(ITM_BASE + 0x40010)) = SWOPrescaler; /* "Async Clock Prescaler Register". Scale the baud rate of the asynchronous output */
+  *((volatile unsigned *)(ITM_BASE + 0x00FB0)) = 0xC5ACCE55; /* ITM Lock Access Register, C5ACCE55 enables more write access to Control Register 0xE00 :: 0xFFC */
+  ITM->TCR = ITM_TCR_TraceBusID_Msk | ITM_TCR_SWOENA_Msk | ITM_TCR_SYNCENA_Msk | ITM_TCR_ITMENA_Msk; /* ITM Trace Control Register */
+  ITM->TPR = ITM_TPR_PRIVMASK_Msk; /* ITM Trace Privilege Register */
+  ITM->TER = portBits; /* ITM Trace Enable Register. Enabled tracing on stimulus ports. One bit per stimulus port. */
+  *((volatile unsigned *)(ITM_BASE + 0x01000)) = 0x400003FE; /* DWT_CTRL */
+  *((volatile unsigned *)(ITM_BASE + 0x40304)) = 0x00000100; /* Formatter and Flush Control Register */
+}
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
+char strs[][6] = { "vbat", "ibat", "iinv", "ifpga" };
 
 /* USER CODE END 0 */
 
@@ -81,19 +101,43 @@ int main(void)
   SystemClock_Config();
 
   /* USER CODE BEGIN SysInit */
-
+  SWO_Init();
   /* USER CODE END SysInit */
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
+  MX_I2C1_Init();
   /* USER CODE BEGIN 2 */
-
+  if (ADS1115_Init(&hi2c1, ADS1115_DATA_RATE_128, ADS1115_PGA_TWO)) {
+    HAL_Delay(1500);
+  }
+  else {
+    HAL_GPIO_WritePin(LED2_GPIO_Port, LED2_Pin, GPIO_PIN_RESET);
+  }
+  uint8_t ADC_VBAT_CHANNEL = ADS1115_MUX_AIN0; // 400 (5 V) -> 1600 (19-20 V) factor 0.08 [1.8/(20+1.8)]
+  uint8_t ADC_IBAT_CHANNEL = ADS1115_MUX_AIN1; // 
+  uint8_t ADC_IINV_CHANNEL = ADS1115_MUX_AIN2;
+  uint8_t ADC_IFPGA_CHANNEL = ADS1115_MUX_AIN3;
+  uint8_t arr[] = {ADC_VBAT_CHANNEL, ADC_IBAT_CHANNEL, ADC_IINV_CHANNEL, ADC_IFPGA_CHANNEL};
+  float values[] = {0,0,0,0};
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
   while (1)
   {
+    for (int ch_i=0; ch_i < ARRAY_SIZE(strs); ch_i++) {
+	    if (ADS1115_readSingleEnded(arr[ch_i], &values[ch_i]) == HAL_OK) {
+        // Voltage successfully read
+	    }
+      else {
+        printf("ERROR\r\n");
+        break;
+      }
+      HAL_Delay(100);
+      printf("[%s : %.2f]", strs[ch_i], values[ch_i]);
+    }
+    printf("\r\n");
     HAL_Delay(1000);
     HAL_GPIO_TogglePin(LED2_GPIO_Port, LED2_Pin);
     /* USER CODE END WHILE */
@@ -145,6 +189,40 @@ void SystemClock_Config(void)
 }
 
 /**
+  * @brief I2C1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_I2C1_Init(void)
+{
+
+  /* USER CODE BEGIN I2C1_Init 0 */
+
+  /* USER CODE END I2C1_Init 0 */
+
+  /* USER CODE BEGIN I2C1_Init 1 */
+
+  /* USER CODE END I2C1_Init 1 */
+  hi2c1.Instance = I2C1;
+  hi2c1.Init.ClockSpeed = 100000;
+  hi2c1.Init.DutyCycle = I2C_DUTYCYCLE_2;
+  hi2c1.Init.OwnAddress1 = 0;
+  hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+  hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+  hi2c1.Init.OwnAddress2 = 0;
+  hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+  hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+  if (HAL_I2C_Init(&hi2c1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN I2C1_Init 2 */
+
+  /* USER CODE END I2C1_Init 2 */
+
+}
+
+/**
   * @brief GPIO Initialization Function
   * @param None
   * @retval None
@@ -157,6 +235,7 @@ static void MX_GPIO_Init(void)
 
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(LED2_GPIO_Port, LED2_Pin, GPIO_PIN_RESET);
@@ -173,6 +252,14 @@ static void MX_GPIO_Init(void)
 }
 
 /* USER CODE BEGIN 4 */
+int _write(int le, char *ptr, int len) {
+  int DataIdx;
+  for(DataIdx = 0; DataIdx < len; DataIdx++)
+  {
+    ITM_SendChar(*ptr++);
+  }
+  return len;
+}
 
 /* USER CODE END 4 */
 
